f99531726f7724bb6366dda79e976b6dd99c5af9
miikey <miikey@na.na>
2023-12-27 14:26:47 +0300

    fix: skip links in code blocks in 'convert all links to mdlink' and 'convert links in folder'

--- commands/ConvertLinksInFolderCommand.ts
+++ commands/ConvertLinksInFolderCommand.ts
@@ -1,6 +1,6 @@
 import { Editor } from "obsidian";
 import { CommandBase, Func, ICommand } from "./ICommand"
-import { LinkData, LinkTypes, findLink, findLinks } from "../utils";
+import { LinkData, LinkTypes, Position, findCodeBlocks, findLink, findLinks } from "../utils";
 import { IObsidianProxy } from "./IObsidianProxy";
 import { ConvertToMdlinkCommandBase } from "./ConvertToMdlinkCommandBase";
 import { TextBuffer } from "../TextBuffer";
@@ -40,37 +40,52 @@ export class ConvertLinksInFolderCommand extends ConvertToMdlinkCommandBase {
 
 		const parentFolder = activeView.file.parent;
 		const files = this.obsidianProxy.Vault.getFilesInFolder(parentFolder);
-		if(!files){
+		if (!files) {
 			this.callback?.(null)
 			return;
 		}
-			(async () => {
-				try {
-					for (let file of files) {
-						if (file.extension !== 'md') {
-							continue;
-						}
-						const content = await this.obsidianProxy.Vault.read(file);
-						const links = findLinks(content, LinkTypes.Wiki);
-						if (links.length > 0) {
-							const textBuffer = new TextBuffer(content);
-							for (let i = links.length - 1; i >= 0; i--) {
-								const link = links[i];
-								//TODO: refactor
-								await this.convertLinkToMarkdownLink1(link, textBuffer, false)
-							}
-							const updatedContent = textBuffer.getValue();
-							await this.obsidianProxy.Vault.modify(file, updatedContent);
-						}
+		(async () => {
+			try {
+				for (let file of files) {
+					if (file.extension !== 'md') {
+						continue;
 					}
-					this.obsidianProxy.createNotice(`${files.length} notes processed.`);
-					this.callback?.(null);
+					const content = await this.obsidianProxy.Vault.read(file);
+					//TODO: fix regex and move inside of findLinks
+					const codeBlocks = findCodeBlocks(content);
+
+					const insideCodeBlock = (pos: Position) => {
+						for (const block of codeBlocks) {
+							if (pos.start > block.position.end) {
+								return false
+							} else if (pos.start >= block.position.start && pos.end <= block.position.end) {
+								return true;
+							}
+						}
+
+						return false;
+					}
+
+					const links = findLinks(content, LinkTypes.Wiki).filter(x => !insideCodeBlock(x.position));
+					if (links.length > 0) {
+						const textBuffer = new TextBuffer(content);
+						for (let i = links.length - 1; i >= 0; i--) {
+							const link = links[i];
+							//TODO: refactor
+							await this.convertLinkToMarkdownLink1(link, textBuffer, false)
+						}
+						const updatedContent = textBuffer.getValue();
+						await this.obsidianProxy.Vault.modify(file, updatedContent);
+					}
 				}
-				catch (err) {
-					this.obsidianProxy.createNotice(`Failed: ${err}`);
-					console.log(err);
-					this.callback?.(err);
-				}
-			})();
+				this.obsidianProxy.createNotice(`${files.length} notes processed.`);
+				this.callback?.(null);
+			}
+			catch (err) {
+				this.obsidianProxy.createNotice(`Failed: ${err}`);
+				console.log(err);
+				this.callback?.(err);
+			}
+		})();
 	}
 }
